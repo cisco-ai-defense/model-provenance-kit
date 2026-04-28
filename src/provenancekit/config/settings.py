1f7fe0bcda8fc23917c600a4fe75a05c207a8b60
Sanket Mendapara <sanket@cisco.com>
2026-04-28 16:18:02 -0400

    feat: Adding more model's feature files (#2)
    * Adding more model's feature files
    
    * Updating catalog
    
    * updating sha
    
    * removing blurp

--- src/provenancekit/config/settings.py
+++ src/provenancekit/config/settings.py
@@ -43,7 +43,7 @@ class Settings(BaseSettings):
     hf_dataset_repo: str = "cisco-ai/model-provenance-kit"
     hf_deep_signals_url: str = ""
     hf_deep_signals_sha256: str = (
-        "497e4e881c63564765fb1237b0764ac4df1e3e80d475dc8e84cad2d3ee936d11"
+        "b94040b668bea03e44b95a69c8554046d68fe0325b54ce64141bbf40765e48e9"
     )
 
     def model_post_init(self, __context: object) -> None:
